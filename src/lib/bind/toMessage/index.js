c70f0deec1fce76e7352fdd07b6fe5098665b42d
Simon Jefford <si@ibrokethat.com>
2016-01-29 16:45:31 +0000

    fixed the error logging

--- src/lib/bind/toMessage/index.js
+++ src/lib/bind/toMessage/index.js
@@ -3,6 +3,8 @@
 const amqp = require('supermyx');
 const value = require('useful-value');
 const co = require('co');
+const e = require('../../core/errors');
+
 
 module.exports = function* toMessage (CONF, cfg) {
 
@@ -16,20 +18,74 @@ module.exports = function* toMessage (CONF, cfg) {
 
         broker.subscribe(c.channel, (message, ack) => {
 
-            const {ctx, params} = message;
-
-            return co(function* () {
-
-                try {
-                    ack();
-                    return yield handler(cfg, ctx, params);
-
+            let logMsg = {
+                event: 'cmd-server:subscriber',
+                data: {
+                    channel: c.channel,
+                    handler: c.resource,
+                    time: {
+                        start: Date.now()
+                    }
                 }
-                catch (ex) {
-                    let logMsg = { level: 'error', event: 'Subscriber Error', data: ex };
-                    process.emit('worker:log', logMsg);
+            };
+
+            co(function* () {
+
+                const {ctx, params} = message;
+
+                yield handler(ctx, params);
+
+            }).then(() => {
+
+                ack();
+
+                logMsg.data.status = 'success';
+                logMsg.data.time.end = Date.now();
+
+                process.emit('worker:log', logMsg);
+
+            }).catch((err) => {
+
+                let error = err;
+
+                switch (true) {
+
+                    case (err instanceof e.InvalidParamsError):
+
+                        error = new e.BadRequestError(err.errors);
+                        break;
+
+                    case (err instanceof e.InvalidDataError):
+
+                        error = new e.InternalServerError(err);
+                        break;
+
+                    case (err instanceof e.InvalidReturnsError):
+
+                        error = new e.InternalServerError(err);
+                        break;
+
+                    case (err instanceof e.InvalidTransformError):
+
+                        error = new e.InvalidTransformError(err);
+                        break;
+
+                    case (!(err instanceof e.ExtendableError)):
+
+                        error = new e.InternalServerError(err);
+                        break;
                 }
+
+                logMsg.level = 'error';
+                logMsg.data.status = error.status;
+                logMsg.data.error = error.messages;
+                logMsg.data.stack = error.stackTraces;
+                logMsg.data.time.end = Date.now();
+
+                process.emit('worker:log', logMsg);
+
             });
+
         });
     }
 
