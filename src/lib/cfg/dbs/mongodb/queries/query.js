b1bdec60073ff1e53120122dee7267f88a173bae
Simon Jefford <si@ibrokethat.com>
2016-02-25 14:01:02 +0000

    updated sort on query

--- src/lib/cfg/dbs/mongodb/queries/query.js
+++ src/lib/cfg/dbs/mongodb/queries/query.js
@@ -1,14 +1,17 @@
 'use strict';
 
+const value = require('useful-value');
 const curry = require('@ibrokethat/curry');
 const requireDir = require('require-dir');
 const helpers = requireDir('../helpers');
 
 module.exports = curry(function* query (db, collection, params, ...args) {
 
+    let orderBy = value(args, '0.orderBy') || value(args, '1.orderBy') || {};
+
     args.unshift(params);
 
-    let items = yield db.collection(collection).find(...args).toArray();
+    let items = yield db.collection(collection).find(...args).sort(orderBy).toArray();
 
     for (let item of items) {
 
